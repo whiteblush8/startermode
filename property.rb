6da765ce5f6032af2e95392755ae140b8ee2e929
Carol Rhyu <75534058+whiteblush8@users.noreply.github.com>
2025-01-13 10:07:58 +0000

    this is for the apartment template

--- property.rb
+++ property.rb
@@ -1,1 +1,89 @@
+require_relative "apartment"
 
+class Duplex
+  def initiliaze  
+    @apartments = []
+  end
+
+  def open
+      puts "Welcome to the apartment!"
+
+
+      loop do
+        if @properties.empty?
+          puts 'The apartment is unavailable. Want to type "book" to look for another place or type exit to leave?'
+        else 
+          puts 'Type "book", "search", or "list" to use the apartment or type exit to leave'
+        end
+
+        job = gets.chomp
+
+        case job
+
+        when "book"
+            log()
+            apartment_list(@type, @location, @price)
+
+
+        when "search"
+            log()
+            apartment_search(@type, @location, @price)
+
+
+        when "list"
+            options()
+
+        when "exit"
+          puts "Talk to you later."
+          break
+
+        else 
+          puts "Did you type something wrongg?"
+
+        end
+
+      end
+
+    end
+
+
+    private
+
+    def apartment_list(type, location, price)
+      apartment = Apartment.new(type, location, price)
+      @apartments.push(apartment)
+      puts "#{apartments.to_s} has been listed."
+    end
+
+    def apartment_search(type, location, price)
+      apartment = @apartments.find { |apartment| apartment.to_s }
+
+    @apartments.delete(apartment)
+
+    puts "#{apartment.to_s} has been searched."
+
+    end
+
+    def log()
+      puts "enter type"
+      @type = gets.chomp
+      @location = gets.chomp
+      puts "enter location"
+      @price = gets.chomp
+      puts "enter price"
+
+    end
+
+
+    def options()
+      if @apartments.empty?
+        "No apartments fit your criteria at this time."
+
+      else 
+        @apartments.each do |apartment|
+          puts apartment.to_s
+
+        end
+      end
+    end
+  end
