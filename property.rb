938383c1607214d10948b86d308923bdcae25f09
Carol Rhyu <75534058+whiteblush8@users.noreply.github.com>
2025-01-15 14:17:25 +0000

    refactor code

--- property.rb
+++ property.rb
@@ -1,89 +1,77 @@
 require_relative "apartment"
 
 class Duplex
-  def initiliaze  
+  def initialize  
     @apartments = []
   end
 
   def open
-      puts "Welcome to the apartment!"
+    puts "Welcome to the apartment!"
 
-
-      loop do
-        if @properties.empty?
-          puts 'The apartment is unavailable. Want to type "book" to look for another place or type exit to leave?'
-        else 
-          puts 'Type "book", "search", or "list" to use the apartment or type exit to leave'
-        end
-
-        job = gets.chomp
-
-        case job
-
-        when "book"
-            log()
-            apartment_list(@type, @location, @price)
-
-
-        when "search"
-            log()
-            apartment_search(@type, @location, @price)
-
-
-        when "list"
-            options()
-
-        when "exit"
-          puts "Talk to you later!"
-          break
-
-        else 
-          puts "Did you type something wrong?"
-
-        end
-
+    loop do
+      if @apartments.empty?
+        puts 'The apartment is unavailable. Want to type "book" to look for another place or type "exit" to leave?'
+      else 
+        puts 'Type "book", "search", or "list" to use the apartment or type "exit" to leave'
       end
 
-    end
+      job = gets.chomp
 
-
-    private
-
-    def apartment_list(type, location, price)
-      apartment = Apartment.new(type, location, price)
-      @apartments.push(apartment)
-      puts "#{apartment.to_s} has been listed."
-    end
-
-    def apartment_search(type, location, price)
-      apartment = @apartments.find { |apartment| apartment.to_s }
-
-     @apartments.delete(apartment)
-
-    puts "#{apartment.to_s} has been searched."
-
-    end
-
-    def log()
-      puts "enter type"
-      @type = gets.chomp
-      @location = gets.chomp
-      puts "enter location"
-      @price = gets.chomp
-      puts "enter price"
-
-    end
-
-
-    def options()
-      if @apartments.empty?
-        "No apartments are available at this time."
-
+      case job
+      when "book"
+        log
+        apartment_list(@type, @location, @price)
+      when "search"
+        log
+        apartment_search(@type, @location, @price)
+      when "list"
+        options
+      when "exit"
+        puts "Talk to you later!"
+        break
       else 
-        @apartments.each do |apartment|
-          puts apartment.to_s
-
-        end
+        puts "Did you type something wrong?"
       end
     end
   end
+
+  private
+
+  def apartment_list(type, location, price)
+    apartment = Apartment.new(type, location, price)
+    @apartments.push(apartment)
+    puts "#{apartment.to_s} has been listed."
+  end
+
+  def apartment_search(type, location, price)
+    apartment = @apartments.find do |apartment|
+      apartment.type == type && apartment.location == location && apartment.price == price
+    end
+
+    if apartment
+      @apartments.delete(apartment)
+      puts "#{apartment.to_s} has been searched and removed."
+    else
+      puts "No apartment found matching those criteria."
+    end
+  end
+
+  def log
+    puts "Enter type"
+    @type = gets.chomp
+    puts "Enter location"
+    @location = gets.chomp
+    puts "Enter price"
+    @price = gets.chomp
+  end
+
+  def options
+    if @apartments.empty?
+      puts "No apartments are available at this time."
+    else 
+      @apartments.each do |apartment|
+        puts apartment.to_s
+      end
+    end
+  end
+end
