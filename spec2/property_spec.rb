f1baee48a4f6f3912d2907b21642dedc481a202b
Carol Rhyu <75534058+whiteblush8@users.noreply.github.com>
2025-01-21 17:58:04 +0000

    test again

--- spec2/property_spec.rb
+++ spec2/property_spec.rb
@@ -1,6 +1,6 @@
-# spec/apartment_spec.rb
+# spec2/apartment_spec.rb
 
-require_relative '../property'  # Adjust the path if necessary
+require_relative '../property'  # Adjust this path if necessary
 
 RSpec.describe Apartment do
   before(:each) do
@@ -10,53 +10,75 @@ RSpec.describe Apartment do
 
   describe "#to_s" do
     it "returns a string with apartment details" do
+      # Set some properties on the apartment
       @apartment.type = "studio"
-      @apartment.location = "new york city"
+      @apartment.location = "New York"
       @apartment.price = "$1200"
 
-      expect(@apartment.to_s).to eq("Apartment type: studio, Location: new york city, Price: $1200")
+      # Check if the to_s method returns the expected output
+      expect(@apartment.to_s).to eq("Apartment type: studio, Location: New York, Price: $1200")
     end
   end
 
   describe "#apartment_list" do
     it "adds an apartment to the list" do
-      @apartment.log
+      # Set up apartment data
+      @apartment.type = "studio"
+      @apartment.location = "Los Angeles"
+      @apartment.price = "$2000"
+
+      # Call apartment_list to add the apartment
       @apartment.apartment_list(@apartment.type, @apartment.location, @apartment.price)
 
+      # Check if the apartment was added to the @apartments array
       expect(@apartment.instance_variable_get(:@apartments).size).to eq(1)
     end
   end
 
   describe "#apartment_search" do
     it "removes an apartment from the list when found" do
-      @apartment.log
+      # Add an apartment to the list
+      @apartment.type = "1B"
+      @apartment.location = "Los Angeles"
+      @apartment.price = "$2000"
       @apartment.apartment_list(@apartment.type, @apartment.location, @apartment.price)
 
+      # Search and remove the apartment
       @apartment.apartment_search(@apartment.type, @apartment.location, @apartment.price)
 
+      # Verify that the apartment was removed
       expect(@apartment.instance_variable_get(:@apartments).size).to eq(0)
     end
 
     it "does not remove any apartment if not found" do
-      @apartment.log
+      # Add an apartment to the list
+      @apartment.type = "1B"
+      @apartment.location = "Los Angeles"
+      @apartment.price = "$2000"
       @apartment.apartment_list(@apartment.type, @apartment.location, @apartment.price)
 
       # Search for an apartment that doesn't exist
-      @apartment.apartment_search("2B", "los angeles", "$1500")
+      @apartment.apartment_search("2B", "Chicago", "$1500")
 
+      # Verify that the apartment is still in the list
       expect(@apartment.instance_variable_get(:@apartments).size).to eq(1)
     end
   end
 
   describe "#options" do
     it "lists all available apartments" do
-      @apartment.log
+      # Add an apartment to the list
+      @apartment.type = "studio"
+      @apartment.location = "New York"
+      @apartment.price = "$1200"
       @apartment.apartment_list(@apartment.type, @apartment.location, @apartment.price)
 
-      expect { @apartment.options }.to output("Apartment type: #{@apartment.type}, Location: #{@apartment.location}, Price: #{@apartment.price}\n").to_stdout
+      # Expect the correct output to be printed when options is called
+      expect { @apartment.options }.to output("Apartment type: studio, Location: New York, Price: $1200\n").to_stdout
     end
 
     it "outputs 'No apartments are available at this time.' if no apartments are listed" do
+      # When no apartments have been added, calling options should print the "No apartments" message
       expect { @apartment.options }.to output("No apartments are available at this time.\n").to_stdout
     end
   end
