fb16aa717b0b2a9e60c64a2439ffee7ddaa157b0
Carol Rhyu <75534058+whiteblush8@users.noreply.github.com>
2025-01-21 20:00:54 +0000

    Double @@apartments

--- property.rb
+++ property.rb
@@ -1,11 +1,12 @@
 class Apartment
   attr_accessor :type, :location, :price
 
+  @@apartments = [] # Class variable to store all apartments
+
   def initialize(type = nil, location = nil, price = nil)
     @type = type
     @location = location
     @price = price
-    @apartments = []
   end
 
   def to_s
@@ -16,13 +17,13 @@ class Apartment
     puts "Welcome to the apartment!"
 
     loop do
-      if @apartments.empty?
-        puts 'The apartment is unavailable. Want to type "book" to look for another place or type "exit" to leave?'
-      else 
-        puts 'Type "book", "search", or "list" to use the apartment or type "exit" to leave'
+      if @@apartments.empty?
+        puts 'The apartment is unavailable. Type "book" to list a new apartment or "exit" to leave.'
+      else
+        puts 'Type "book", "search", or "list" to manage apartments or "exit" to leave.'
       end
 
-      job = gets.chomp
+      job = gets.chomp.downcase.strip
 
       case job
       when "book"
@@ -34,10 +35,10 @@ class Apartment
       when "list"
         options
       when "exit"
-        puts "Talk to you later!"
+        puts "Goodbye!"
         break
-      else 
-        puts "Did you type something wrong?"
+      else
+        puts "Invalid input. Please try again."
       end
     end
   end
@@ -45,44 +46,59 @@ class Apartment
   private
 
   def apartment_list(type, location, price)
-    apartment = Apartment.new(type, location, price)
-    @apartments.push(apartment)
-    puts "#{apartment.to_s} has been listed."
+    if type && location && price
+      apartment = Apartment.new(type, location, price)
+      @@apartments.push(apartment)
+      puts "#{apartment.to_s} has been listed."
+    else
+      puts "Invalid apartment details. Listing failed."
+    end
   end
 
   def apartment_search(type, location, price)
-    apartment = @apartments.find do |apartment|
-      apartment.type == type && apartment.location == location && apartment.price == price
+    apartment = @@apartments.find do |apt|
+      apt.type.downcase == type.downcase &&
+      apt.location.downcase == location.downcase &&
+      apt.price.to_s == price.to_s
     end
 
     if apartment
-      @apartments.delete(apartment)
-      puts "#{apartment.to_s} has been searched and removed."
+      @@apartments.delete(apartment)
+      puts "#{apartment.to_s} has been found and removed."
     else
-      puts "No apartment found matching those criteria."
+      puts "No matching apartment found."
     end
   end
 
   def log
-    puts "Enter type"
-    @type = gets.chomp
-    puts "Enter location"
-    @location = gets.chomp
-    puts "Enter price"
-    @price = gets.chomp
+    puts "Enter type:"
+    @type = gets.chomp.strip
+    puts "Enter location:"
+    @location = gets.chomp.strip
+    puts "Enter price (numeric):"
+    @price = gets.chomp.strip
+
+    unless valid_price?(@price)
+      puts "Invalid price entered. Please try again."
+      @price = nil
+    end
+  end
+
+  def valid_price?(price)
+    price.match?(/^\d+(\.\d{1,2})?$/) # Validates numeric prices
   end
 
   def options
-    if @apartments.empty?
+    if @@apartments.empty?
       puts "No apartments are available at this time."
-    else 
-      @apartments.each do |apartment|
-        puts apartment.to_s
+    else
+      @@apartments.each_with_index do |apartment, index|
+        puts "#{index + 1}. #{apartment.to_s}"
       end
     end
   end
 end
 
-# To run the program, you would call the `open` method on an instance of the Apartment class.
+# To run the program:
 apartment = Apartment.new
 apartment.open
